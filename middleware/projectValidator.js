cfa235697fd74a7c25bbeaf700edce57efc7a1af
Elyas Arkin <(elyas.arkin@hotmail.se)>
2020-11-27 19:25:33 +0100

    upda

--- middleware/projectValidator.js
+++ middleware/projectValidator.js
@@ -1,6 +1,7 @@
 const {check,validatorresult } = require('express-validator');
-const { async } = require('q');
-// const Project1 = require("../Models/projectModel");
+
+
+
  const checkV = [
   check("name").notEmpty().withMessage("project name can not be empty"),
   check("url").notEmpty().withMessage("url can not be empty"),
