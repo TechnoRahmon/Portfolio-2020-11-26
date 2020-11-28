0d5cdfeea35cfb6f586e3fb35199894e71c81fbf
Abdulrahman <Abulrahmanmuhialdeen@hotmail.com>
2020-11-28 10:55:11 +0100

    spelling correction

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -1,4 +1,6 @@
 
+
+
 /**  PROJECT ACTIONS **/
 export const GET_PROJECTS = 'GET_PROJECTS'
 export const ADD_PROJECT = 'ADD_PROJECT'
@@ -7,8 +9,9 @@ export const DELETE_PROJECT = 'DELETE_PROJECT'
 export const ERROR_PROJECT = 'ERROR_PROJECT'
 
 /** RESUME ACTIONS  **/
-export const GET_RESUME = 'GET_RESUME'
+export const GET_RESUMES = 'GET_RESUME'
 export const ADD_RESUME = 'ADD_RESUME'
 export const  GET_RESUME_DETAILS = 'GET_RESUME_DETAILS'
 export const  DELETE_RESUME = 'DELETE_RESUME'
 export const  ERROR_PROJECT = 'ERROR_PROJECT'
+
